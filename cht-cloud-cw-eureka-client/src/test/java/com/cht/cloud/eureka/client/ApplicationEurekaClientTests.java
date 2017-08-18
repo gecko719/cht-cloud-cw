91594782d9076aec4a9dea3409147886ad17887f
chungweilin <chungweilin@cht.com.tw>
2017-08-18 16:15:17 +0800

    Init Proj cht-cloud-cw

---
+++ cht-cloud-cw-eureka-client/src/test/java/com/cht/cloud/eureka/client/ApplicationEurekaClientTests.java
@@ -0,0 +1,16 @@
+package com.cht.cloud.eureka.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationEurekaClientTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
