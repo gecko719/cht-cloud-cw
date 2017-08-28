1d0be14849cc14bdfb3cf487556801a28cffda0a
gecko <geckomail@gmail.com>
2017-08-28 13:46:58 +0800

    Add Module cht-cloud-cw-config-server

---
+++ cht-cloud-cw-config-server/src/main/java/com/cht/cloud/config/ChtCloudCwConfigServerApplication.java
@@ -0,0 +1,12 @@
+package com.cht.cloud.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChtCloudCwConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChtCloudCwConfigServerApplication.class, args);
+	}
+}
