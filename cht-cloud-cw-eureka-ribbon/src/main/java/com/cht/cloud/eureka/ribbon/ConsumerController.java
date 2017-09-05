3627983bf990e5b3255c29025dec19b64c3802b0
gecko <geckomail@gmail.com>
2017-09-06 00:58:08 +0800

    enchancement

--- cht-cloud-cw-eureka-ribbon/src/main/java/com/cht/cloud/eureka/ribbon/ConsumerController.java
+++ cht-cloud-cw-eureka-ribbon/src/main/java/com/cht/cloud/eureka/ribbon/ConsumerController.java
@@ -4,13 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ConsumerController {
-
-    @Autowired
-    private RestTemplate restTemplate;
 
     @Autowired
     private ConsumerService consumerService;
